03e48aff64d342a6a3a8a4e4c9b5a891e4a6cf80
Jonatan Sánchez <jona2255@gmail.com>
2021-02-22 21:11:24 +0100

    Añadido ejercicio 2 y cambio en la estructura del 1

--- js/index.js
+++ js/index.js
@@ -1,9 +1,4 @@
-const numeros = [1, 4, 9, 16];
-
-const numerosModificados = mapJona(numeros, x => x * 2);
-
-console.log(numerosModificados);
-
+/* Ejercicio 1 */
 function mapJona(numeros, cb) {
   const numerosModificados = [];
   for (const numero of numeros) {
@@ -11,3 +6,19 @@ function mapJona(numeros, cb) {
   }
   return numerosModificados;
 }
+const numeros = [1, 4, 9, 16];
+const numerosModificados = mapJona(numeros, x => x * 2);
+console.log(numerosModificados);
+/* Ejercicio 2 */
+function filterJona(palabras, cb) {
+  const palabrasModificadas = [];
+  for (const palabra of palabras) {
+    if (cb(palabra)) {
+      palabrasModificadas.push(palabra);
+    }
+  }
+  return palabrasModificadas;
+}
+const palabras = ["spray", "limit", "elite", "exuberant", "destruction", "present"];
+const palabrasModificadas = palabras.filter(palabra => palabra.length > 6);
+console.log(palabrasModificadas);
